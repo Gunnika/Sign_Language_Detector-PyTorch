d7078d19b2c06c72082c03c4d5ca9c9b0afd5d86
kautukkundan <kautukkundan@gmail.com>
2020-03-16 20:33:03 +0530

    cleaned up code, changed ROI in image

--- app/webstreaming.py
+++ app/webstreaming.py
@@ -120,7 +120,6 @@ def predictor(img):
     
 
 # import the necessary packages
-from pyimagesearch.motion_detection import SingleMotionDetector
 from imutils.video import VideoStream
 from flask import Response
 from flask import Flask
@@ -154,56 +153,6 @@ time.sleep(2.0)
 def index():
 	# return the rendered template
 	return render_template("index.html")
-
-# def detect_motion(frameCount):
-# 	# grab global references to the video stream, output frame, and
-# 	# lock variables
-# 	global vs, outputFrame, lock
-
-# 	# initialize the motion detector and the total number of frames
-# 	# read thus far
-# 	md = SingleMotionDetector(accumWeight=0.1)
-# 	total = 0
-
-# 	# loop over frames from the video stream
-# 	while True:
-# 		# read the next frame from the video stream, resize it,
-# 		# convert the frame to grayscale, and blur it
-# 		frame = vs.read()
-# 		frame = imutils.resize(frame, width=400)
-# 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# 		gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-# 		# grab the current timestamp and draw it on the frame
-# 		timestamp = datetime.datetime.now()
-# 		cv2.putText(frame, timestamp.strftime(
-# 			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-# 			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-# 		# if the total number of frames has reached a sufficient
-# 		# number to construct a reasonable background model, then
-# 		# continue to process the frame
-# 		if total > frameCount:
-# 			# detect motion in the image
-# 			motion = md.detect(gray)
-
-# 			# check to see if motion was found in the frame
-# 			if motion is not None:
-# 				# unpack the tuple and draw the box surrounding the
-# 				# "motion area" on the output frame
-# 				(thresh, (minX, minY, maxX, maxY)) = motion
-# 				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-# 					(0, 0, 255), 2)
-		
-# 		# update the background model and increment the total number
-# 		# of frames read thus far
-# 		md.update(gray)
-# 		total += 1
-
-# 		# acquire the lock, set the output frame, and release the
-# 		# lock
-		# with lock:
-		# 	outputFrame = frame.copy()
             
 def detect_gesture(frameCount):
 
@@ -223,17 +172,20 @@ def detect_gesture(frameCount):
         if frame is not None: 
 
             frame = cv2.flip(frame, 1)
-            frame = cv2.resize( frame, (400,400) )
+            width = 720
+            height = 480
+            
+            frame = cv2.resize( frame, (width,height))
 
-            cv2.rectangle(frame, (300,300), (100,100), (0,255,0), 2)
+            cv2.rectangle(frame, (width//2, 0), (width, height), (0,255,0), 2)
 
-            crop_img = frame[100:300, 100:300]
+            crop_img = frame[0:height, width//2:width]
             grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
 
             thresh = cv2.threshold(grey,210,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
 
-            blackboard = np.zeros(frame.shape, dtype=np.uint8)
-            cv2.putText(blackboard, "Predicted text - ", (30, 40), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 0))
+            # blackboard = np.zeros(frame.shape, dtype=np.uint8)
+            cv2.putText(frame, "Predicted text - ", (30, 40), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 0))
             if count_frames > 20 and pred_text != "":
                 total_str += pred_text
                 count_frames = 0
@@ -242,15 +194,21 @@ def detect_gesture(frameCount):
                 old_text = pred_text
                 pred_text = predictor(thresh)
 
+                print('inside')
+
                 if old_text == pred_text:
                     count_frames += 1
                 else:
                     count_frames = 0
-                cv2.putText(blackboard, total_str, (30, 80), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 127))
+                cv2.putText(frame, total_str, (30, 80), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 127))
+
+            keypress = cv2.waitKey(1)
+            if keypress == ord('c'):
+                flag = True
             
-            res = np.hstack((frame, blackboard))
+            # res = np.hstack((frame, blackboard))
             with lock:
-                outputFrame = res.copy()
+                outputFrame = frame.copy()
 
 		
 def generate():
